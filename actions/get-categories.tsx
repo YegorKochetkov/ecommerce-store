1e7ab77d1c6f1316ea4f13c6e323fdba9469fb9a
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-10-11 18:28:16 +0300

    style: improve layout, add empty line

--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -4,5 +4,6 @@ const storeUrl = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
 export const getCategories = async (): Promise<Category[]> => {
   const res = await fetch(storeUrl);
+
   return res.json();
 };
--- components/billboard.tsx
+++ components/billboard.tsx
@@ -9,14 +9,14 @@ const Billboard = ({ data }: { data: BillboardType }) => {
         style={{ backgroundImage: `url(${data.imageUrl})` }}
         className={`
           rounded-xl overflow-hidden aspect-square sm:aspect-[5/2]
-          bg-center bg-cover bg-no-repeat
+          bg-center bg-cover bg-no-repeat p-3
         `}
       >
         <div className='w-full h-full grid place-items-center'>
           <h1
             className={`
-            font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs
-            p-3 sm:p-4 lg:p-6 rounded-xl bg-white bg-opacity-30
+            font-bold text-3xl sm:text-4xl lg:text-5xl sm:max-w-xl max-w-xs
+            p-3 sm:p-4 lg:p-6 rounded-xl bg-white bg-opacity-40
           `}
           >
             {data.label}
