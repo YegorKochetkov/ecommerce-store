2c7599a0584907bcf0a4452fd1d410c2a5ec382e
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-10-13 10:16:23 +0300

    chore: minor code style improve

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -7,17 +7,13 @@ const Billboard = ({ data }: { data: BillboardType }) => {
     <article>
       <div
         style={{ backgroundImage: `url(${data.imageUrl})` }}
-        className={`
-          rounded-xl overflow-hidden aspect-square sm:aspect-[5/2] lg:aspect-[9/3] 2xl:aspect-[10/3]
-          bg-center bg-cover bg-no-repeat p-3
-        `}
+        className='rounded-xl overflow-hidden aspect-square sm:aspect-[5/2]
+          lg:aspect-[9/3] 2xl:aspect-[10/3] bg-center bg-cover bg-no-repeat p-3'
       >
         <div className='w-full h-full grid place-items-center'>
           <h1
-            className={`
-            font-bold text-3xl sm:text-4xl lg:text-5xl
-            p-3 sm:p-4 lg:p-6 rounded-xl bg-white bg-opacity-40
-          `}
+            className='font-bold text-3xl sm:text-4xl lg:text-5xl
+            p-3 sm:p-4 lg:p-6 rounded-xl bg-white bg-opacity-40'
           >
             {data.label}
           </h1>
