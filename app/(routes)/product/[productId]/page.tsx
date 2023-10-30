27f3b42077227e302c4aa92299abf82b932743ce
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-10-30 21:06:11 +0200

    fix: hide current product from suggested product

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,37 +1,39 @@
-import React from "react";
+import React from 'react';
 
-import { getProduct } from "@/actions/get-product";
-import { getProducts } from "@/actions/get-products";
+import { getProduct } from '@/actions/get-product';
+import { getProducts } from '@/actions/get-products';
 
-import ProductList from "@/components/product-list";
-import Gallery from "@/components/gallery";
-import Info from "@/components/info";
+import ProductList from '@/components/product-list';
+import Gallery from '@/components/gallery';
+import Info from '@/components/info';
 
 const ProductPage = async ({ params }: { params: { productId: string } }) => {
-  const product = await getProduct(params.productId);
-  const suggestedProducts = await getProducts({
-    categoryId: product.category.id,
-  });
+	const product = await getProduct(params.productId);
+	const suggestedProducts = (
+		await getProducts({
+			categoryId: product.category.id,
+		})
+	).filter((p) => p.id !== product.id);
 
-  return (
-    <main className='bg-white py-6 px-4 sm:px-6 sm:py-10 lg:px-8'>
-      <div className='container mx-auto'>
-        <section className='px-4 pb-10 sm:px-6 lg:px-8'>
-          <div
-            className='flex flex-col-reverse sm:flex-col gap-y-4 lg:grid
+	return (
+		<main className='bg-white py-6 px-4 sm:px-6 sm:py-10 lg:px-8'>
+			<div className='container mx-auto'>
+				<section className='px-4 pb-10 sm:px-6 lg:px-8'>
+					<div
+						className='flex flex-col-reverse sm:flex-col gap-y-4 lg:grid
               lg:grid-cols-2 lg:items-start sm:gap-x-8'
-          >
-            <Gallery images={product.images} productName={product.name} />
-            <div className='px-4 sm:mt-10 sm:px-0 lg:mt-0'>
-              <Info data={product} />
-            </div>
-          </div>
-        </section>
-        <hr className='mb-10' />
-        <ProductList title='You may also like' items={suggestedProducts} />
-      </div>
-    </main>
-  );
+					>
+						<Gallery images={product.images} productName={product.name} />
+						<div className='px-4 sm:mt-10 sm:px-0 lg:mt-0'>
+							<Info data={product} />
+						</div>
+					</div>
+				</section>
+				<hr className='mb-10' />
+				<ProductList title='You may also like' items={suggestedProducts} />
+			</div>
+		</main>
+	);
 };
 
 export default ProductPage;
