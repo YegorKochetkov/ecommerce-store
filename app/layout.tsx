d19759ef5f6f59cd4aa255657ef399be3c259653
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-11-29 22:13:39 +0200

    feat: add toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,9 @@ import { Urbanist } from 'next/font/google';
 
 import Footer from '@/components/footer';
 import Navbar from '@/components/navbar';
+
 import ModalProvider from '@/providers/modal-provider';
+import ToastProvider from '@/providers/toast-provider';
 
 import './globals.css';
 
@@ -23,6 +25,7 @@ export default function RootLayout({
 		<html lang='en'>
 			<body className={font.className}>
 				<ModalProvider />
+				<ToastProvider />
 				<Navbar />
 				{children}
 				<Footer />
