6b6d3463ce2af125e5bd85039028b573ad06a48e
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-11-29 22:13:55 +0200

    feat: add cart store

---
+++ hooks/use -cart.ts
@@ -0,0 +1,50 @@
+import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+
+import toast from 'react-hot-toast/headless';
+
+import { Product } from '@/types';
+
+type CartStore = {
+	items: Product[];
+	addItem: (item: Product) => void;
+	removeItem: (id: string) => void;
+	removeAll: () => void;
+};
+
+const useCart = create(
+	persist<CartStore>(
+		(set, get) => ({
+			items: [],
+			addItem: (data: Product) => {
+				const currentItems = get().items;
+				const existingItem = currentItems.find((item) => item.id === data.id);
+
+				if (existingItem) {
+					return toast('Item already in cart');
+				}
+
+				set({
+					items: [...currentItems, data],
+				});
+				toast.success('Item added to cart');
+			},
+			removeItem: (id: string) => {
+				set({
+					items: get().items.filter((item) => item.id !== id),
+				});
+				toast.success('Item removed from cart');
+			},
+			removeAll: () => {
+				set({
+					items: [],
+				});
+				toast.success('All items removed from cart');
+			},
+		}),
+
+		{ name: 'cart-store', storage: createJSONStorage(() => localStorage) },
+	),
+);
+
+export default useCart;
