e4ce4b6f12a6458c90b40528343500423244c4c4
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2024-03-13 15:05:04 +0200

    buidl: update billboard id

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import Billboard from '@/components/billboard';
-import ProductList from '@/components/product-list';
-import { getBillboard } from '@/actions/get-billboard';
-import { getProducts } from '@/actions/get-products';
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import { getBillboard } from "@/actions/get-billboard";
+import { getProducts } from "@/actions/get-products";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
 	const products = await getProducts({ isFeatured: true });
-	const billboard = await getBillboard('5a923d9e-a49d-49d8-bbb7-121c1c2b20e7');
+	// TODO: move billboard id to .env
+	const billboard = await getBillboard("5d083d51-a365-4ee5-b0ad-4cbe33e34067");
 
 	return (
-		<main className='py-8 px-4 sm:px-6 lg:px-8'>
-			<div className='container m-auto grid gap-y-10'>
+		<main className="py-8 px-4 sm:px-6 lg:px-8">
+			<div className="container m-auto grid gap-y-10">
 				<Billboard data={billboard} />
-				<ProductList title='Featured Products' items={products} />
+				<ProductList title="Featured Products" items={products} />
 			</div>
 		</main>
 	);
